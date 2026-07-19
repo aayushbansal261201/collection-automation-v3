2f08c3bd9f51a8d347bd7f7df06e679d062810fa
aayushbansal261201 <128252510+aayushbansal261201@users.noreply.github.com>
2026-07-20 00:42:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -46,10 +46,10 @@ MAX_RETRIES = 3
 # ===============================
 
 REPORTS = {
-    "Payment Reconciliation": "http://172.235.29.24:9001/public/question/1c093dc3-8a95-4508-a157-18d71e4b2044.xlsx",
-    "Exception Report": "http://172.235.29.24:9001/public/question/d16d3ab3-e778-485c-9328-8b3104b73ae2.xlsx",
-    "Monthly LAN Activity": "http://172.235.29.24:9001/public/question/ab98d982-f732-45de-b621-6462fd45005c.xlsx",
-    "Daily Activity": "http://172.235.29.24:9001/public/question/bb03c9db-00f1-4eeb-9203-e2e5ee520aa9.xlsx",
-    "Receipt": "http://172.235.29.24:9001/public/question/ddfe0b02-18fb-4019-82e3-e88b936e6359.xlsx",
-    "Lat-Long": "http://172.235.29.24:9001/public/question/f941da96-d887-4668-b55a-e2f8784d55a4.xlsx"
+    "Payment Reconciliation": "dummy1.xlsx",
+    "Exception Report": "dummy2.xlsx",
+    "Monthly LAN Activity": "http://172.235.29.24:9001/public/question/ab98.xlsx",
+    "Daily Activity": "http://172.235.29.24:9001/public/question/bb03c9db-00f1-4eeb-9.xlsx",
+    "Receipt": "http://172.235.29.24:9001/public/question/ddfe0b02-18fb-4019-82e3-e.xlsx",
+    "Lat-Long": "http://172.235.29.24:9001/public/question/f941da96-d887-4668-b55a-e.xlsx"
 }
